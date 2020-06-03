22fb299ed7733e8f42da83cabd05f579c3fa96f2
mopeneko <26901342+mopeneko@users.noreply.github.com>
2020-06-03 20:43:08 +0900

    トップページを作成

---
+++ web/layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app v-model="isDrawerEnabled"></v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click.stop="isDrawerEnabled = !isDrawerEnabled"></v-app-bar-nav-icon>
+      <v-toolbar-title>NovelGamest</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-content>
+
+    <v-footer app></v-footer>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      isDrawerEnabled: false
+    }
+  }
+})
+</script>
---
+++ web/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-row justify="center">
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <p class="display-1 text-center">NovelGamest</p>
+        <p class="text-center">ノベルゲーマーで築く新世界</p>
+        <div class="text-center"><v-btn>はじめる</v-btn></div>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-card>
+          <v-card-title>NovelGamest とは</v-card-title>
+          <v-card-text>
+            <p>ノベルゲームの感想を投げ合うWebサービスです。</p>
+            <p>ノベルゲームの共有を容易に行えるものを目指しております。</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center">
+      <v-col cols="12" md="6" lg="6" xl="6">
+        <v-card>
+          <v-card-title>連絡先</v-card-title>
+          <v-card-text>
+            <p>開発者 <a href="https://lem0n.cc">もぺねこ</a> まで。</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
